843d1ae145180a4bbc7358cab4f01bf419f7c39f
Ahmedlebshten <ahmedlebshtenlebshten@gmail.com>
2026-02-08 14:46:16 +0200

    update backend.tf file

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "jenkins-terraform-backend-9904"
-    key     = "eks/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
+    bucket       = "jenkins-terraform-backend-731628759499"
+    key          = "eks/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
     use_lockfile = true
   }
 }
